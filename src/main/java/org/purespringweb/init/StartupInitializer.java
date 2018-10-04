30be1737eaf747a50d35882b09bb05c74436821f
Dmitry Nikiforov <dmitry.nikiforov@adition.com>
2018-10-04 17:43:11 +0200

    Move from root to app

--- src/main/java/org/purespringweb/init/StartupInitializer.java
+++ src/main/java/org/purespringweb/init/StartupInitializer.java
@@ -32,6 +32,6 @@ public class StartupInitializer extends AbstractAnnotationConfigDispatcherServle
 
     @Override
     protected String[] getServletMappings() {
-        return new String[] { "/" };
+        return new String[] { "/app/*" };
     }
 }
