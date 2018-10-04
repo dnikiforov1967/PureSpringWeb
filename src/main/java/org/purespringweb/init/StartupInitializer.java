ea82163eef6130a7def141fd54140e87e3eedd03
Dmitry Nikiforov <dmitry.nikiforov@adition.com>
2018-10-04 16:21:30 +0200

    Change path mapping to root

--- src/main/java/org/purespringweb/init/StartupInitializer.java
+++ src/main/java/org/purespringweb/init/StartupInitializer.java
@@ -32,6 +32,6 @@ public class StartupInitializer extends AbstractAnnotationConfigDispatcherServle
 
     @Override
     protected String[] getServletMappings() {
-        return new String[] { "/app/*" };
+        return new String[] { "/" };
     }
 }
