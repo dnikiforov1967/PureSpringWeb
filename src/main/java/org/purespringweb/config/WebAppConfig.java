29410706949ed2fa9221a4abc3b888e0d6c6a27c
Dmitry Nikiforov <dmitry.nikiforov@adition.com>
2018-10-04 16:22:11 +0200

    Create initial configuration, allow MVC, allow scan and create resolver bean

--- src/main/java/org/purespringweb/config/WebAppConfig.java
+++ src/main/java/org/purespringweb/config/WebAppConfig.java
@@ -5,13 +5,36 @@
  */
 package org.purespringweb.config;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 /**
  *
  * @author dnikiforov
  */
 @Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"org.purespringweb.beans.web"})
 public class WebAppConfig {
+	
+	private final static Logger LOG = Logger.getLogger(WebAppConfig.class.getName());
+	
+    @Bean
+    public ViewResolver viewResolver() {
+		LOG.log(Level.INFO, "ViewResolver to instantiate");
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+ 
+        return viewResolver;
+    }	
 	
 }
