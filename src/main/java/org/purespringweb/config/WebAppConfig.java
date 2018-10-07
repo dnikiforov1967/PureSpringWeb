89a9c2e87fc9c880da13df7abba0116c07678bd0
dnikiforov1967 <dnikiforov1967@gmail.com>
2018-10-07 18:31:12 +0200

    Configure and initialize CommonsFileResolver

--- src/main/java/org/purespringweb/config/WebAppConfig.java
+++ src/main/java/org/purespringweb/config/WebAppConfig.java
@@ -12,6 +12,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -39,6 +41,12 @@ public class WebAppConfig implements WebMvcConfigurer {
  
         return viewResolver;
     }	
+    
+    @Bean
+    public MultipartResolver multipartResolver() {
+            final CommonsMultipartResolver mr = new CommonsMultipartResolver();
+            return mr;
+    }
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
