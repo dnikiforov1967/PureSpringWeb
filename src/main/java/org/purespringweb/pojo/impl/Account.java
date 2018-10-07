3d447971569e8155722810b525b94ad5b80121ad
dnikiforov1967 <dnikiforov1967@gmail.com>
2018-10-07 10:45:46 +0200

    Append class to get as the return value

---
+++ src/main/java/org/purespringweb/pojo/impl/Account.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.purespringweb.pojo.impl;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author dima
+ */
+public class Account {
+    Integer id;
+
+    public Account() {
+    }
+
+    public Account(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Account other = (Account) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+}
